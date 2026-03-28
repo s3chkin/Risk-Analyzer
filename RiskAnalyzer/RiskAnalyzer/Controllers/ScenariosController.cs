2afb8ad4b8c82e30c8504455b6fc3ad7a638637f
s3chkin <seckins191@gmail.com>
2026-03-28 16:15:15 +0200

    add functionality for scenarios - add, edit, delete, details

---
+++ RiskAnalyzer/RiskAnalyzer/Controllers/ScenariosController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RiskAnalyzer.Data;
+using RiskAnalyzer.Data.Models;
+using RiskAnalyzer.Models;
+
+namespace RiskAnalyzer.Controllers
+{
+    public class ScenariosController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        public ScenariosController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            var model = db.Scenarios.Select(s => new InputScenariosModel
+            {
+                Id = s.Id,
+                Title = s.Title,
+                Description = s.Description,
+                Location = s.Location,
+                RiskTypeName = s.RiskType.Name,
+                Status = s.Status
+
+            }).ToList();
+
+            return View(model);
+        }
+
+        public IActionResult Add()
+        {
+            var riskTpyes = db.RiskTypes.Select(rt => new SelectListItem
+            {
+                Value = rt.Id.ToString(),
+                Text = rt.Name.ToString()
+            }).ToList();
+            var model = new InputScenariosModel
+            {
+                RiskTypes = riskTpyes
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Add(InputScenariosModel model)
+        {
+            
+                var scenario = new Scenarios
+                {
+                    Title = model.Title,
+                    Description = model.Description,
+                    Location = model.Location,
+                    CreatedAt = model.CreatedAt,
+                    Status = model.Status,
+                    RiskTypeId = model.RiskTypeId
+                };
+                db.Scenarios.Add(scenario);
+                db.SaveChanges();
+            
+            return this.RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var model = db.Scenarios.Where(s => s.Id == id).Select(s => new InputScenariosModel
+            {
+                Id = s.Id,
+                Title = s.Title,
+                Description = s.Description,
+                Location = s.Location,
+                CreatedAt = s.CreatedAt,
+                Status = s.Status,
+                RiskTypeName = s.RiskType.Name
+            }).FirstOrDefault();
+            return View(model);
+        }
+
+
+        public IActionResult Edit(int id)
+        {
+            var scenario = db.Scenarios.FirstOrDefault(s => s.Id == id);
+            var model = new InputScenariosModel
+            {
+                Id = scenario.Id,
+                Title = scenario.Title,
+                Description = scenario.Description,
+                Location = scenario.Location,
+                CreatedAt = scenario.CreatedAt,
+                Status = scenario.Status,
+                RiskTypeId = scenario.RiskTypeId
+            };
+
+            var riskTpyes = db.RiskTypes.Select(rt => new SelectListItem
+            {
+                Value = rt.Id.ToString(),
+                Text = rt.Name
+            }).ToList();
+            model.RiskTypes = riskTpyes;
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(InputScenariosModel model)
+        {
+           
+                var scenario = db.Scenarios.FirstOrDefault(s => s.Id == model.Id);
+                scenario.Title = model.Title;
+                scenario.Description = model.Description;
+                scenario.Location = model.Location;
+                scenario.CreatedAt = model.CreatedAt;
+                scenario.Status = model.Status;
+                scenario.RiskTypeId = model.RiskTypeId;
+                db.Scenarios.Update(scenario);
+                db.SaveChanges();
+           
+            return this.RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var scenario = db.Scenarios.FirstOrDefault(s => s.Id == id);
+            if (scenario != null)
+            {
+                db.Scenarios.Remove(scenario);
+                db.SaveChanges();
+            }
+            return this.RedirectToAction("Index");
+        }
+    }
+}
---
+++ RiskAnalyzer/RiskAnalyzer/Models/InputScenariosModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RiskAnalyzer.Data.Models;
+
+namespace RiskAnalyzer.Models
+{
+    public class InputScenariosModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Location { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string Status { get; set; } = "Нов";
+        public int RiskTypeId { get; set; }
+        [Required]
+        public string RiskTypeName { get; internal set; }
+        public List<SelectListItem> RiskTypes { get; set; } = new List<SelectListItem>();
+    }
+}
